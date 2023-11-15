a0c9846cf26563a8934341df3256862816096ecb
Agus27111 <agussetiawan.belajar@gmail.com>
2023-11-16 02:39:28 +0700

    device mobile

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    colors: {
+      primary: "#4d545c",
+      secondary: "#9eabb3",
+      btn1: "#26baa4",
+      btn2: "#6173ff",
+      white: "#fff",
+    },
+  },
+  plugins: [],
+};
